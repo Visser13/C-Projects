c6a7801524041862d8384f4f52fb15c4feb97cfb
Visser13 <agough.grim@gmail.com>
2026-06-02 18:01:58 -0400

    Finshed the malloc video by Bro Code

--- main.c
+++ main.c
@@ -4,17 +4,14 @@
 
 int main(){
 
-    bool isOlnine = true;
+int num1 = 1;
+int num2 = 10;
+int num3 = 100;
+
+printf("%d\n",num1);
+printf("%d\n", num2);
+printf("%d\n",num3);
 
 
-    if(isOlnine){
-
-        printf("Derrick is ONLINE");
-    }
-    else{
-
-        printf("Derrick is OFFLINE");
-    }
-
-    return 0;
+   return 0; 
 }
